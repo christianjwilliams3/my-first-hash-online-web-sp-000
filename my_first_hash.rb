64df07dae25020a4cc7f0faed118349be4c55bf3
christian williams <christian.williamsj@gmail.com>
2019-09-09 22:02:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,6 +21,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    shipping_manifest["muskets"] = "2", ["gun powder"] = 4
+    shipping_manifest["muskets"] = "2"
 shipping_manifest
+end
+    shipping_manifest["gun powder"] = 4
+shipping_manifest
+end
 end
